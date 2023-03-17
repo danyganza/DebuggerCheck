a3420936ad9da8013004c0534869f23e4c5b3027
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-16 18:57:37 -0600

    added

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+
+let username =  "ganza";
+let user_age = 25;
+let is_member = true;
+
+
+if (username === "admin" || (user_age >= 30 && is_member)) {
+  console.log("hello team!");
+} else {
+  console.log("Welcome, enjoy!");
+}
+
+
+
+
+console.log("Hello " + username + "!");
+
+console.log("You are: " + user_age);
+
+
+const max_volume = 100;
+const current_volume = 50;
+
+console.log(max_volume / current_volume);
+console.log((max_volume / current_volume) * 100);
+
